da672348ba136b58a30decf5f687788011eef508
kyumin0411 <rbalsgood@gmail.com>
2022-04-19 10:27:31 +0900

    [dev] WIP : 날짜 입력 후 데이터 받아오도록 구현

--- src/App.js
+++ src/App.js
@@ -4,23 +4,62 @@ import './App.css';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import axios from 'axios';
+import * as dayjs from 'dayjs';
 
 function App() {
+	// let response;
 	const [response, setResponse] = useState('');
+	let date;
+	// let date = dayjs().subtract(1, 'day').format('YYYY-MM-DD');
+	// const [date, setDate] = useState(dayjs().subtract(1, 'day').format('YYYY-MM-DD'));
 
 	useEffect(async () => {
-		async function fetchData() {
-			await axios({
-				method: 'GET',
-				url: `/parse?searchDate=2022-04-16`,
-			}).then((response) => setResponse(response));
-		}
-		fetchData();
+		date = dayjs().subtract(1, 'day').format('YYYY-MM-DD');
+
+		await axios({
+			method: 'GET',
+			url: `/parse?searchDate=${date}`,
+		}).then((res) => setResponse(res));
 	}, []);
 
-	console.log('after useEffect? ', response);
+	const handleChange = async (e) => {
+		date = e.target.value;
+		// setDate(e.target.value);
 
-	return <div className='mainText'>코로나 확진 상태</div>;
+		console.log('in handleChange() date: ', date);
+		await axios({
+			method: 'GET',
+			url: `/parse?searchDate=${date}`,
+		}).then((res) => setResponse(res));
+		console.log('in handleChange() response : ', response);
+	};
+
+	const deathCount = response?.data?.payload?.deathCount.toLocaleString();
+	const decideCount = response?.data?.payload?.decideCount.toLocaleString();
+	const errMessage = !response?.data?.payload ? response?.data?.message : null;
+
+	return (
+		<div className='textframe'>
+			<div className='mainText'>코로나 19 감염 현황 조회</div>
+			<br />
+			<label>
+				검색 날짜:
+				<input
+					onChange={(e) => {
+						handleChange(e);
+					}}
+					type='date'
+					value={date ?? dayjs().subtract(1, 'day').format('YYYY-MM-DD')}
+				/>
+			</label>
+			<div className='smallText'>
+				<br></br> {errMessage}
+			</div>
+			<br />
+			<div>사망자 수 : {deathCount ?? '-'} 명</div>
+			<div>확진자 수 : {decideCount ?? '-'} 명</div>
+		</div>
+	);
 }
 
 export default App;
